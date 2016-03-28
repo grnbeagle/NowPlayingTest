e93b77f0da456094d7cd2199b12b6159d1d115c4
Amie Kweon <akweon@pandora.com>
2016-03-28 16:20:28 -0700

    Initial Commit

---
+++ NowPlayingTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NowPlayingTest
+//
+//  Created by Amie Kweon on 3/28/16.
+//  Copyright © 2016 Amie Kweon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

97a02683e4ad7c1f44128eced89d06f7ce040577
Amie Kweon <akweon@pandora.com>
2016-03-30 17:45:40 -0700

    Initial commit

--- NowPlayingTest/ViewController.swift
+++ NowPlayingTest/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import MediaPlayer
+import AVKit
 
 class ViewController: UIViewController {
 
+    var toggle = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Try using AVAudioSession
+//        do {
+//            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//        } catch {
+//            NSLog("ERROR with audio session")
+//        }
+
+        // Try using AVPlayer
+        let player = AVPlayer(URL: NSURL(string: "http://www.ebookfrenzy.com/ios_book/movie/movie.mov")!)
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        self.addChildViewController(playerController)
+        self.view.addSubview(playerController.view)
+        playerController.view.frame = self.view.frame
+
+        player.play()
+
+        UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
+        updateNowPlaying()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onButtonClick(sender: AnyObject) {
+        updateNowPlaying()
+    }
 
+    func updateNowPlaying() {
+        let playbackRate: Double = toggle ? 0.0 : 1.0
+        NSLog(">>> playbackRate: \(playbackRate)")
+        let info: [String: AnyObject] = [
+            MPMediaItemPropertyTitle: "My Title",
+            MPMediaItemPropertyArtist: "My Artist",
+            MPMediaItemPropertyAlbumTitle: "My Title",
+            MPNowPlayingInfoPropertyElapsedPlaybackTime: 100.0,
+            MPNowPlayingInfoPropertyPlaybackRate: playbackRate
+        ]
+        MPNowPlayingInfoCenter.defaultCenter().nowPlayingInfo = info
+        toggle = !toggle
+    }
 }
 
